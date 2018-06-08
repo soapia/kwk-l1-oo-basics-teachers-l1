94fb152dc691568cc4cbc361e6e4e7a64cc1dc3f
sofia ongele <sofiaongele@gmail.com>
2018-06-08 16:43:32 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
 # book.rb
 # Add you Book class here
+
+class Book # define the class
+  
+  def initialize(title, author)
+    @title = title
+    @author = author
+  end
+  
+  def title
+    @title
+  end
+  
+  def author
+    @author
+  end
+  
+  
+  
+end
